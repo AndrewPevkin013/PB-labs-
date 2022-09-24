1d768bff8952937ffbe8ca3794711b55cb55ed3a
AndrewPevkin013 <112945923+AndrewPevkin013@users.noreply.github.com>
2022-09-24 12:39:34 +0300

    Mod lab 2

--- Lab_2.c
+++ Lab_2.c
@@ -1,4 +1,3 @@
-
 #include "stdio.h"
 #include "math.h"
 int main(void){
@@ -8,6 +7,6 @@ int main(void){
     alpha = alpha*pi/180;  //Перевели с градусов в радианы
     z1 = cos(alpha) + sin(alpha) + cos(3*alpha) + sin(3*alpha);
     z2 = 2* sqrt(2) * cos(alpha) * sin(pi/4 + 2 * alpha);
-    printf_s("%.3f\t %.3f\n", z1, z2);
+    printf("%.3f\t %.3f\n", z1, z2);
     return 0;
 }
