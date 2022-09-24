c737fccb788a09155a31adb4fac13f3de48e45b5
AndrewPevkin013 <112945923+AndrewPevkin013@users.noreply.github.com>
2022-09-24 12:33:12 +0300

    Added Lab 2

---
+++ Lab_2.c
@@ -0,0 +1,13 @@
+
+#include "stdio.h"
+#include "math.h"
+int main(void){
+    double alpha, z1, z2, pi = 3.1415926;
+
+    scanf_s("%lf", &alpha);
+    alpha = alpha*pi/180;  //Перевели с градусов в радианы
+    z1 = cos(alpha) + sin(alpha) + cos(3*alpha) + sin(3*alpha);
+    z2 = 2* sqrt(2) * cos(alpha) * sin(pi/4 + 2 * alpha);
+    printf_s("%.3f\t %.3f\n", z1, z2);
+    return 0;
+}
